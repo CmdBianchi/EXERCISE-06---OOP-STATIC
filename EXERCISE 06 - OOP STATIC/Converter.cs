4e732fbfd481d998f86c723c56f577636182b320
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-26 15:10:57 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 06 - OOP STATIC/Converter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_06___OOP_STATIC{
+    //----------------------START | OBJ CONVERTER------------------------//
+    class Converter{
+        public static double Iof = 6;
+        public static double DolarToReal(double amount, double price) {
+            double total = amount * price;
+            return total + (total * Iof / 100);
+        }
+    }
+    //---------------------------- END ---------------------------------//
+}
---
+++ EXERCISE 06 - OOP STATIC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EXERCISE_06___OOP_STATIC{
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {      
+        static void Main(string[] args) {
+            Console.Write("Qual cotação do dólar? "); double price = double.Parse(Console.ReadLine());          
+            Console.Write("Quantos dólares ira comprar? "); double amount = double.Parse(Console.ReadLine());
+            double result = Converter.DolarToReal(amount, price);           
+            Console.Write("Valor a ser pago em R$: " +result.ToString("F2"));
+            Console.ReadLine();
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
